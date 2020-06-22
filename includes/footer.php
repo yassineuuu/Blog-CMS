eed256638756f9934fea2d8510ce4e82dd940813
yassineuuu <57989575+yassineuuu@users.noreply.github.com>
2020-06-23 00:20:59 +0100

    first comit

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+    <footer>
+    <nav>
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+
+        <p>Web site &copy; 2020</p>
+    </footer>
+
+</body>
+</html>
